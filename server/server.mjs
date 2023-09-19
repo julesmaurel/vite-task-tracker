ae7a82361aea8b011f0825e45e180f2488c805e1
Jules Maurel <julesmaurel34@gmail.com>
2023-09-19 18:23:14 +0200

    make sure db is connected before listening

--- server/server.mjs
+++ server/server.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import router from "./routes/tasks.mjs";
+import db from "./db/conn.mjs";
 
 const PORT = process.env.PORT || 3003;
 const app = express();
@@ -11,7 +12,11 @@ app.use(express.json());
 
 app.use("/tasks", router);
 
+if(db){
 // start the Express server
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
+} else {
+  throw new Error('db not connected')
+}
