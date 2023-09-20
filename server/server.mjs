240c3e383660e6d0967974fec740360a3a2dad44
Jules Maurel <julesmaurel34@gmail.com>
2023-09-20 10:02:39 +0200

    serve FE from server

--- server/server.mjs
+++ server/server.mjs
@@ -1,4 +1,6 @@
 import express from "express";
+import path from 'path';
+import { fileURLToPath } from 'url';
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import router from "./routes/tasks.mjs";
@@ -7,6 +9,9 @@ import db from "./db/conn.mjs";
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const corsOptions = {
   origin: ["http://localhost:3000" , process.env.CYCLIC_URL],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -14,10 +19,21 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.static('./frontend/dist'));
-
 
 app.use("/tasks", router);
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 if(db){
 // start the Express server
